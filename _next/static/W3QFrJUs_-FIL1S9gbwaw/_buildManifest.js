7ad6233f12210d95c1af353134833f7bd864dc75
ebonsignori <evanabonsignori@gmail.com>
2021-12-19 05:57:36 +0000

    Update from https://github.com/Ebonsignori/my-sites/commit/811ff962fcc524f5368ed120fbe49e468ddbc7e6

---
+++ _next/static/W3QFrJUs_-FIL1S9gbwaw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ef37a019eaa29608.js"],"/_error":["static/chunks/pages/_error-18c58bcef08a9b71.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
